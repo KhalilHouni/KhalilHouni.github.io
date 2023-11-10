c2a4616bd130a9adc20b3dd831e891c8dfa0bd79
KhalilHouni <132214784+KhalilHouni@users.noreply.github.com>
2023-11-10 15:23:58 +0100

    Update wheel.js

--- secret mini game/wheel.js
+++ secret mini game/wheel.js
@@ -1,8 +1,8 @@
 const wheel = document.getElementById("wheel");
+const pointer = document.getElementById("pointer");
 const button = document.querySelector("button");
 
-const colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#FFD700", "#8B4513"];
-const words = ["Khalil", "Arthur", "Clara", "Lena", "Sabri", "Lucas", "Léo","Mame","Remy","Neïs","Adrien","Maud","Encadrant du jour"];
+const words = ["Khalil", "Arthur", "Clara", "Lena", "Sabri", "Lucas", "Léo", "Mame", "Remy", "Neïs", "Adrien", "Maud", "Encadrant du jour"];
 
 let spinning = false;
 
@@ -12,22 +12,56 @@ function spinWheel() {
   if (!spinning) {
     spinning = true;
 
-    // Randomly select a color
-    const randomColorIndex = Math.floor(Math.random() * colors.length);
-    const selectedColor = colors[randomColorIndex];
-    const selectedPhrase = phrases[randomColorIndex];
+    // Randomly select a rotation angle
+    const rotationAngle = Math.floor(Math.random() * 360);
 
     // Rotate the wheel
-    const rotationAngle = randomColorIndex * (360 / colors.length);
     wheel.style.transition = "transform 3s ease-out";
     wheel.style.transform = `rotate(${rotationAngle}deg)`;
 
-    // Wait for the animation to complete and show the alert
+    // Rotate the pointer in the opposite direction
+    pointer.style.transition = "transform 3s ease-out";
+    pointer.style.transform = `translate(-50%, -50%) rotate(${-rotationAngle}deg)`;
+
+    // Create triangles for each word
+    const triangleContainer = document.createElement("div");
+    triangleContainer.className = "wheel";
+
+    for (let i = 0; i < words.length; i++) {
+      const triangle = document.createElement("div");
+      triangle.className = "triangle";
+      const startAngle = (360 / words.length) * i;
+      const endAngle = (360 / words.length) * (i + 1);
+      triangle.style.background = `conic-gradient(from ${startAngle}deg to ${endAngle}deg, #fff 0%, #FFD700 50%, #fff 100%)`;
+      triangle.style.borderLeftWidth = "200px";
+      triangle.style.borderRightWidth = "200px";
+      triangle.style.borderBottomWidth = "346.4px";
+      triangle.style.borderLeftColor = "transparent";
+      triangle.style.borderRightColor = "transparent";
+      triangle.style.borderBottomColor = "transparent";
+      triangle.style.transform = `rotate(${(360 / words.length) * i}deg)`;
+      triangleContainer.appendChild(triangle);
+    }
+
+    // Replace the existing wheel with triangles
+    wheel.innerHTML = "";
+    wheel.appendChild(triangleContainer);
+
+    // Wait for the animation to complete
     setTimeout(() => {
-      alert(`${selectedPhrase} va demarrer la cloture aujourd'hui`);
       wheel.style.transition = ""; // Remove transition for future spins
       wheel.style.transform = ""; // Reset the rotation
+      pointer.style.transition = ""; // Remove transition for future spins
+      pointer.style.transform = ""; // Reset the rotation
       spinning = false;
+
+      // Randomly select a word
+      const randomWordIndex = Math.floor(Math.random() * words.length);
+      const selectedWord = words[randomWordIndex];
+
+      // Show the selected word in an alert
+      alert(`${selectedWord} va démarrer la cloture aujourd'hui`);
     }, 3000);
   }
 }
+
