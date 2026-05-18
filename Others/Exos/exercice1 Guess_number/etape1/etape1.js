82045acfc9a2f6a79302dcbb658f5fc9aee36f83
Khalil Houni <132214784+KhalilHouni@users.noreply.github.com>
2026-05-18 16:02:35 +0200

    Delete Others directory

--- Others/Exos/exercice1 Guess_number/etape1/etape1.js
+++
@@ -1,7 +0,0 @@
-function demanderNombre() {
-    var givenNumber = prompt("Entrez un nombre :");
-    return givenNumber;
-}
-
-var givenNumber = demanderNombre();
-alert("Vous avez entré le nombre : " + givenNumber);
